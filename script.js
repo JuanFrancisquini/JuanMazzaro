a2ce2be582bdfff2c4e5a28866adb19979239d6f
MazzaAlpha <90626179+MazzAlpha@users.noreply.github.com>
2022-10-11 13:17:49 -0300

    up

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ function enterMenu(){
         itens.style.display = 'none'} else { itens.style.display = 'block'
     }
 }
+
+function me(elemento){
+    const txtarray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    txtarray.forEach((letra, i) => { setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
+}
+
+
+const texto = document.querySelector('h1');
+me(texto)
